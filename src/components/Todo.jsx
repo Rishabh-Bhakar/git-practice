41f02d2a9e55b593e23615cb7a3d59f8e6ddeae4
Rishabh-Bhakar <bhakarrishabh111@gmail.com>
2026-06-20 15:40:15 +0530

    Changes

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Todo = () => {
+  const [task, settask] = useState([])
+  const [newTask, setnewTask] = useState("")
+  function handChangler(event){
+
+  }
+  function addTask(){
+
+  }
+  function MoveTaskUp(index){
+
+  }
+  function  MoveTaskDown(index){
+
+  }
   return (
     <>
+    <div>
+        <h1>TO-DO-List</h1>
+        
+    </div>
     </>
   )
 }
