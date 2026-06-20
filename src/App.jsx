98e202162d60aa54d569bc5d51547e6d2c030f75
Rishabh-Bhakar <bhakarrishabh111@gmail.com>
2026-06-20 14:58:12 +0530

    Navbar and components added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Header from "./components/Header";
 import Form from "./components/Form";
 
 function App() {
   return (
     <>
-      <Form />
+    <Header/>
+    <Form />
     </>
   );
 }
